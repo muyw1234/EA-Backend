f126472d1c5109e58f72c3a964ffe00891bc3e90
Marcmc1803 <marc.martin.cuartero@estudiantat.upc.edu>
2026-03-19 10:41:52 +0100

    Refactor Autor CRUD to use service pattern and add comments

---
+++ src/services/Autor.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import Autor, {IAutorModel, IAutor} from "../models/Autor";
+
+// Crear un autor nuevo
+const createAutor = async (data: Partial<IAutor>): Promise<IAutorModel> => {
+    const autor = new Autor({
+        _id: new mongoose.Types.ObjectId(),
+        ...data
+    });
+    return await autor.save();
+};
+
+// Buscar un autor por su ID
+const getAutor = async (autorId: string): Promise<IAutorModel | null> => {
+    return await Autor.findById(autorId);
+};
+
+// Listar todos los autores
+const getAllAutores = async (): Promise<IAutorModel[]> => {
+    return await Autor.find();
+};
+
+// Actualizar los datos de un autor
+const updateAutor = async (autorId: string, data: Partial<IAutor>): Promise<IAutorModel | null> => {
+    const autor = await Autor.findById(autorId);
+    if (autor) {
+        autor.set(data);
+        return await autor.save();
+    }
+    return null;
+};
+
+// Borrar un autor (elimina de la DB)
+const deleteAutor = async (autorId: string): Promise<IAutorModel | null> => {
+    return await Autor.findByIdAndDelete(autorId);
+};
+
+export default { createAutor, getAutor, getAllAutores, updateAutor, deleteAutor };
