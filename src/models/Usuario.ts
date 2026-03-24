46fa5369c084e98f5da87483d0e823147138e6c4
Wenjie Chen <wenjie.sab@gmail.com>
2026-03-24 10:45:12 +0000

    Paquete jwt instalado, funciones para verificar y encriptar password de usuario.

--- src/models/Usuario.ts
+++ src/models/Usuario.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs';
 import mongoose, { Document, Schema } from 'mongoose';
 
 export interface IUsuario {
@@ -6,6 +7,8 @@ export interface IUsuario {
     password: string;
     libros: mongoose.Types.ObjectId[] | string[]; // Es un array porque claro, un usuario puede tener mas de un libro
     IsDeleted?: boolean;
+    encryptPassword(password: string): Promise<string>;
+    validatePassword(password: string): Promise<boolean>;
 }
 
 export interface IUsuarioModel extends IUsuario, Document {}
@@ -23,5 +26,14 @@ const UsuarioSchema: Schema = new Schema(
         versionKey: false
     }
 );
+
+UsuarioSchema.methods.encryptPassaword = async (password: string): Promise<string> => {
+    const salt = await bcrypt.genSalt(10); // el algoritmo se aplica 10 veces
+    return bcrypt.hash(password, salt);
+};
+
+UsuarioSchema.methods.validatePassword = async function (password: string): Promise<boolean> {
+    return await bcrypt.compare(password, this.password);
+};
 
 export default mongoose.model<IUsuarioModel>('Usuario', UsuarioSchema);
